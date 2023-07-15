a1337d2339dbdfc55a6bd79da97462fc844472ac
kshetritej <tejghartikshetri@gmail.com>
2023-07-15 09:33:35 +0545

    Done phonebook step 11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,16 @@ import { useState, useEffect } from 'react'
 import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
-import axios from 'axios'
-import { v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import nameService from './services/persons.js'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setSearch] = useState('')
+  const [errorMessage, setErrorMessage] = useState (null)
 
   useEffect(() => {
     nameService
@@ -19,18 +20,18 @@ const App = () => {
         setPersons(initialNames)
       })
   }, [])
-  // useEffect(()=>{
-  //   axios.get('http://localhost:3001/persons').then(response =>{
-  //     setPersons(response.data)
-  //   })
-  // },[])
 
   const addName = (event) => {
     event.preventDefault()
     // checks if person name already exists in the array
     const personExists = persons.find((per) => per.name.toLowerCase() === newName.toLowerCase());
+
     if (personExists) {
-      alert(`${newName} already exists in the phonebook`)
+      const message = `${newName} already exists in Phonebook`
+      setErrorMessage(message)
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },3000)
       return
     }
     const nameObject = {
@@ -38,11 +39,7 @@ const App = () => {
       phone: newNumber,
       id: uuidv4,
     }
-    // axios.post("http://localhost:3001/persons",nameObject).then(response =>{
-    //   setPersons(persons.concat(response.data))
-    //   setNewName('')
-    //   setNewNumber('')
-    // })
+
     nameService.create(nameObject).then(returnedNames => {
       setPersons(persons.concat(returnedNames))
       setNewName('')
@@ -52,7 +49,8 @@ const App = () => {
 
   //Let's filter the names
   const filteredNames = persons.filter((per) =>
-    per.name.toLowerCase().includes(newSearch.toLowerCase()));
+    per.name.includes(newSearch)
+  );
 
   //input handlers
   const handleNameChange = (event) => {
@@ -70,18 +68,21 @@ const App = () => {
 
   //For deleting the entry
   const deleteName = (id) => {
-    const personToDelete = persons[persons.length -1 ].name
+    const personToDelete = persons[persons.length - 1].name
     const addr = window.location.href
     window.confirm(`Delete ${personToDelete} ?`) ?
-    nameService.deleteData(id).then(returnedData => {
-      setPersons(returnedData)
-    }) :
-    window.location.href = addr
+      nameService.deleteData(id).then(returnedData => {
+        setPersons(returnedData)
+      }) :
+      window.location.href = addr
   }
+
+
   //Final Return 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message = {errorMessage} />
       <Filter searchValue={newSearch} onSearchChange={handleSearchValue} filterName={filteredNames} searchTerm={newSearch} />
 
       <PersonForm onSubmitAction={addName} nameValue={newName} onNameChange={handleNameChange} phoneValue={newNumber} onPhoneChange={handleNumberChange} />
