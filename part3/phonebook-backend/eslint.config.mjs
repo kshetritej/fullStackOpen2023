293ad275c04806355beef6490e8103934a6ef477
kshetritej <tejghartikshetri@gmail.com>
2024-05-07 23:15:25 +0545

    add: start script

--- part3/phonebook-backend/eslint.config.mjs
+++ part3/phonebook-backend/eslint.config.mjs
@@ -1,11 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReactConfig,
 ];
--- part3/phonebook-backend/models/contactModel.js
+++ part3/phonebook-backend/models/contactModel.js
@@ -20,8 +20,8 @@ const contactSchema = new mongoose.Schema({
     minLength: 8,
     type:String,
     validate:{
-      validator: function (v){
-        return new RegExp("^\d{2,3}-\d+$").test(v);
+      validator: function (phn){
+        return new RegExp("^\d{2,3}-\d+$").test(phn);
       },
       message: props => `${props.value} is not a valid phone number!`
     }
