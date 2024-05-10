b9ba676eb9e18a2224b3a3e3d634a8e3174c3b90
kshetritej <tejghartikshetri@gmail.com>
2024-05-10 06:36:55 +0545

    test: pass test favouriteBlog(test 2)

--- blog-list/tests/list_helper.test.js
+++ blog-list/tests/list_helper.test.js
@@ -1,6 +1,7 @@
 const { test, describe } = require("node:test");
 const listHelper = require("../utils/list_helper");
 const assert = require('node:assert');
+const biggerList = require('./testBlogs.json')
 
 test("dummy returns one", () => {
   const blogs = [];
@@ -21,25 +22,6 @@ describe("total likes", () => {
     },
   ];
 
-  const biggerList = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
 
   test("of empty list is zero", () => {
     const result = listHelper.totalLikes([]);
@@ -53,6 +35,31 @@ describe("total likes", () => {
 
   test("of a bigger list is calculated right", () => {
     const result = listHelper.totalLikes(biggerList);
-    assert.strictEqual(result, 10);
+    assert.strictEqual(result, 36);
   });
 });
+
+//favourite blog
+
+describe('favourite Blog', () => { 
+  const blogs = biggerList;
+  test('of empty list is empty object', () => {
+    const result = listHelper.favouriteBlog([]);
+    assert.deepStrictEqual(result, {});
+  });
+
+  test('when list has only one blog, equals the blog', () => {
+    const result = listHelper.favouriteBlog(blogs.slice(0, 1));
+    assert.deepStrictEqual(result, blogs[0]);
+  });
+
+  test('when blogs has multiple blogs', () => {
+    const result = listHelper.favouriteBlog(blogs);
+    assert.deepStrictEqual(result, blogs[2]);
+  });
+
+  test('when multiple blogs have the same amount of likes', () => {
+    const result = listHelper.favouriteBlog(biggerList);
+    assert.deepStrictEqual(result, biggerList[2]);
+  });
+ })
--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -12,7 +12,37 @@ const totalLikes = (blogs) => {
   return sum;
 };
 
+const favouriteBlog = (blogs) => {
+  //return blogs with highest likes count
+
+  const likes = blogs.map((blog) => blog.likes);
+
+  const maxLike = Math.max(...likes);
+
+  const favBlog = blogs.filter((blog) => blog.likes === maxLike);
+  if (favBlog.length > 1) {
+    const favBlogtoShow = favBlog[0];
+    return favBlogtoShow;
+  } else if (favBlog.length === 1) {
+    return favBlog[0];
+  }
+  else {
+    return {};
+  }
+};
+
+const mostBlogs = (blogs) => {
+  //returns the author who has the largest amount of blogs
+};
+
+const mostLikes = (blogs) => {
+  //returns the author, whose blog posts have the largest amount of likes.
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes,
 };
