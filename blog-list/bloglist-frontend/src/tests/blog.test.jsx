9b22cc6a0ac1f0dd2c81334f82a904bf2b11e471
kshetritej <tejghartikshetri@gmail.com>
2024-06-08 06:48:31 +0545

    test: semi pass add blog test

--- blog-list/bloglist-frontend/src/tests/blog.test.jsx
+++ blog-list/bloglist-frontend/src/tests/blog.test.jsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { test, expect, vi } from "vitest";
 import Blog from "../components/Blog";
 import { userEvent } from "@testing-library/user-event";
+import BlogForm from "../components/BlogForm";
+import exp from "constants";
 
 test("renders content", () => {
   const blog = {
@@ -50,17 +52,47 @@ test("clicking the like button twice calls event handler twice", async () => {
 
   // const mockHandler = vi.fn();
 
-
-  render(<Blog blog={blog}  />);
+  render(<Blog blog={blog} />);
   const button = screen.getByText("view details");
   expect(button).toBeDefined();
 
   const appUser = userEvent.setup();
   await appUser.click(button);
-  
 
   const likeButton = screen.getByText("like");
   expect(likeButton).toBeDefined();
   await appUser.click(likeButton);
-  
+});
+
+test("that the form calls the event handler it received as props with the right details when a new blog is created", async () => {
+  const blogData = {
+    title: "",
+    url: "",
+  };
+  const handleBlogSubmit = vi.fn();
+  const setBlogData = vi.fn();
+  const setOpen = vi.fn();
+
+  const user = userEvent.setup();
+
+  render(
+    <BlogForm
+      blogData={blogData}
+      handleBlogSubmit={handleBlogSubmit}
+      open={true}
+      setBlogData={setBlogData}
+      setOpen={setOpen}
+    />
+  );
+
+  const titleInput = screen.getByLabelText("Blog Title");
+  const urlInput = screen.getByLabelText("url");
+  const addButton = screen.getByText("add");
+
+  await user.type(titleInput, "testing a form...");
+  await user.type(urlInput, "http://test.com");
+
+  await user.click(addButton);
+
+  expect(handleBlogSubmit.mock.calls).toHaveLength(1);
 });
